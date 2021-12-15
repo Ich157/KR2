6e79d397c76fe587daadb653b90cfcf609c71e19
lefkothea1 <lefkotheapat@gmail.com>
2021-12-15 18:32:46 +0100

    working on slide example

--- test.py
+++ test.py
@@ -2,8 +2,9 @@ from BNReasoner import BNReasoner
 from BayesNet import BayesNet
 import pandas as pd
 
-net = BNReasoner("testing/dog_problem.BIFXML")
+# net = BNReasoner("testing/dog_problem.BIFXML")
+net = BNReasoner("testing/lecture_example.BIFXML")
 variables = net.bn.get_all_variables()
 #print(net.d_seperation(variables[0],variables[1],variables[2]))
-evidence = pd.Series({variables[4]:False})
-net.marginal_distributions(variables[1],evidence)
+# evidence = pd.Series({variables[4]:False})
+# net.marginal_distributions(variables[1],evidence)
