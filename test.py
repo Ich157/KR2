b6d235705489df09889d351c7205ad744106603e
no-breuer <64846139+no-breuer@users.noreply.github.com>
2021-12-17 14:05:43 +0100

    Add files via upload

--- test.py
+++ test.py
@@ -1,10 +1,15 @@
 from BNReasoner import BNReasoner
 from BayesNet import BayesNet
 import pandas as pd
+import os
+os.environ["KMP_DUPLICATE_LIB_OK"]="TRUE"
 
 # net = BNReasoner("testing/dog_problem.BIFXML")
 net = BNReasoner("testing/lecture_example.BIFXML")
 variables = net.bn.get_all_variables()
+#print(variables)
+#print(net.bn.draw_structure())
+#net.bn.BNReasoner.marginal_distributions(variables[3], variables[1])
 #print(net.d_seperation(variables[0],variables[1],variables[2]))
-# evidence = pd.Series({variables[4]:False})
-# net.marginal_distributions(variables[1],evidence)
+evidence = pd.Series({'Winter?':True, 'Sprinkler?':False})
+net.marginal_distributions(['Wet Grass?', 'Slippery Road?'],evidence)
