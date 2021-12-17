da8b4c78cbb7c3df10861c1d98fa3166bd95ba66
Arne <arne.sailer@gmx.net>
2021-12-17 19:52:55 +0100

    import copy

--- test.py
+++ test.py
@@ -1,3 +1,5 @@
+import copy
+
 from BNReasoner import BNReasoner
 from BayesNet import BayesNet
 import pandas as pd
@@ -7,15 +9,15 @@ import itertools
 os.environ["KMP_DUPLICATE_LIB_OK"]="TRUE"
 
 # net = BNReasoner("testing/dog_problem.BIFXML")
-net = BNReasoner("testing/lecture_example2.BIFXML")
-variables = net.bn.get_all_variables()
-print(variables)
+#net = BNReasoner("testing/lecture_example2.BIFXML")
+#variables = net.bn.get_all_variables()
+#print(variables)
 #print(net.bn.draw_structure())
 #net.bn.BNReasoner.marginal_distributions(variables[3], variables[1])
 #print(net.d_seperation(variables[0],variables[1],variables[2]))
-evidence = pd.Series({'O':True})
+#evidence = pd.Series({'O':True})
 #net.marginal_distributions(['Wet Grass?', 'Slippery Road?'],evidence)
-net.map_mpe(['I', 'J'],evidence)
+#net.map_mpe(['I', 'J'],evidence)
 
 
 def create_network_params(n_variables):
