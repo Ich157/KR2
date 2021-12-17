447b646bb47a11e2f2aec2a2013f439f7faa2787
no-breuer <64846139+no-breuer@users.noreply.github.com>
2021-12-17 14:05:03 +0100

    Add files via upload

--- BNReasoner.py
+++ BNReasoner.py
@@ -2,6 +2,9 @@ from typing import Union
 from BayesNet import BayesNet
 import networkx as nx
 import itertools
+import copy
+import pandas as pd
+import numpy as np
 
 
 class BNReasoner:
@@ -48,7 +51,7 @@ class BNReasoner:
         ordering = []
         while len(X):
             degree_order = sorted(list(X), key=lambda n: (g.degree[n], n))
-            #print(degree_order)
+            # print(degree_order)
             node = degree_order[0]
             ordering.append(node)
             node_neighbours = list(g.neighbors(node))
@@ -102,34 +105,154 @@ class BNReasoner:
         self.bn.draw_structure()
         all_cpts = self.bn.get_all_cpts()
         new_cpts = all_cpts
-        print("old cpts")
-        #print(all_cpts)
+        # print("old cpts")
+        # print(all_cpts)
         for cpt in all_cpts:
             new_cpts[cpt] = self.bn.get_compatible_instantiations_table(E, all_cpts[cpt])
-        print("updated cpts")
-        #print(new_cpts)
+        # print("updated cpts")
+        # print(new_cpts)
         variables = self.bn.get_all_variables()
-        variables.remove(Q)
+        for var in Q:
+            variables.remove(var)
         ordering = self.min_degree(variables)
         print(ordering)
         for var in ordering:
-            self.multi_out(new_cpts, var)
+            # print("variable to eliminate:")
+            # print(var)
+            out = self.multi_out(var, new_cpts)
+            summed = self.sum_out(var, out)
+        print(summed)
+        return summed
 
     # def summing_out(self):
 
-    def multi_out(self, cpts, var):
+    def multi_out(self, var, allcpts):
+        """multiplies all factors that refer to a variable.
+            Creates a multiplied(larger) CPT for var. Should it also update it?
+
+                :param  var: str. the name of variable
+                :return: cpt with variables that appear together with var in their cpts (DataFrame)
+                """
+
+        # getting CPTs where var appears on, to be multiplied with each other
+        relavent_cpts = []
+        for cpt in allcpts.values():
+            if cpt.columns.__contains__(var):
+                relavent_cpts.append(cpt)
+
+        # will store unique variable/columns from list of dfs. So will contain all the vars thet the multiplied cpt needs to have
+        var_cols = []
+        for df in relavent_cpts:
+            for col in df.columns:
+                if col not in var_cols:
+                    var_cols.append(col)
+        var_cols.remove('p')
+
+        temp_cpt = pd.DataFrame(list(itertools.product([False, True], repeat=len(var_cols))), columns=var_cols)
+
+        # merging all dfs , according to cols of the right(smallest) one (p cols included) together
+        for df in relavent_cpts:
+            temp_cpt = temp_cpt.merge(df, on=list(df)[:-1], how='right')
+
+        # getting only p col names, to multiply them
+        pcols = list(temp_cpt)
+        for x in var_cols:
+            pcols.remove(x)
+
+        # multiplication product will be stored in pmulti
+        pmulti = pd.Series(data=1.0, index=temp_cpt.index)
+        # multiplying all pcols
+        for pcol in pcols:
+            pmulti = pmulti * temp_cpt[pcol]
+        # creating output
+        outdf = temp_cpt[var_cols]
+        outdf['p'] = pmulti  # throws settingWithCopyWarning- but works ok
+
+        return outdf
+
+    def sum_out(self, var, multipliedCPT):
+        """eliminates var from multipliedCPT by summing out
+
+        :param  var: str. the name of variable
+        :param  multipliedCPT: cpt with multiplied factors where var appears on (DataFrame, output of multi_out function)
+        :return: cpt with var eliminated (DataFrame)
+        """
+        # creating output,without the col of var we want to eliminate
+        dfout = multipliedCPT.copy()  # if we chose to update ct inside multi out function, then we just need to get_cpt(
+        # var) here
+        dfout = dfout.drop(columns=var)
+
+        # getting sum of rows with same truth values(they will correspond to the 2rows where our var to be eliminated is true and F)
+        relevant_var_cols = [i for i in dfout.columns if
+                             i != 'p']  # making sure that it wont group by p column, will also skip columns with p in name!!fix it
+        dfout = dfout.groupby(relevant_var_cols)['p'].sum()  # summing p from cols with same truth values
+        dfout = dfout.reset_index()
+        # print("reduced cpt:\n"+str(dfout))
+
+        return dfout
+
+
+""""
+    def get_rel_cpts(self, cpts, var):
         parents = self.bn.structure.pred[var]
         if len(parents) == 0:
             p = cpts[var]['p']
 
         children_var = self.bn.get_children(var)
-        child_cpts = []
-        for child in children_var:
-            child_cpts.append(self.bn.get_cpt(child))
+        node_cpt = self.bn.get_cpt(var)
+        #print(children_var)
+        if children_var:
+            relevant_cpts = {n: cpts[n] if n in cpts.keys() else cpts[n] for n in children_var}
+            #print(relevant_cpts)
+            for c in children_var:
+                new_cpt = self.multi_out(node_cpt, relevant_cpts[c])
+                del cpts[c]
+            if var in cpts:
+                del cpts[var]
+            new_cpt = self.sum_out(new_cpt, var)
+            return new_cpt
+        else:
+            del cpts[var]
+
+
+    def multi_out(self, node_cpt, cpt):
+
+        col_cpt1 = set(node_cpt.columns[:-1])
+        col_cpt2 = set(cpt.columns[:-1])
+
+        alpha = col_cpt2 - col_cpt1
+        #print(alpha)
+        #print(node_cpt)
+        #print(cpt)
+
+        for a in alpha:
+            index = len(col_cpt1) - 1
+            if {True, False}.issubset(set(cpt[a])):
+                cpt_copy = copy.deepcopy(node_cpt)
+                node_cpt.insert(index, a, True)
+                #print(node_cpt)
+                new = pd.concat([node_cpt, cpt_copy])
+                new = new.replace(np.nan, False, regex=True)  # fill up the rest of the rows with False
+                #print(new)
+
+        # TODO: checkout what needs to be multiplicated with what
+        for index1, index2 in new.iterrows():
+            print(index1)
+            print(index2)
+            for _, rows in cpt.iterrows():
+                #print(rows)
+                if index2[col_cpt1].equals(rows[col_cpt1]):
+                    new.at[index1, 'p'] *= rows['p']
+
+        print(new)
+
+    def sum_out(self, cpt, var):
+        pass
+
         #print("child cpts")
         #print(cpts.items())
         rows_true = cpts[var] == 'True'
-        print(rows_true)
+        #print(rows_true)
         rows_false = cpts.loc[cpts[var] == False]
 
         print(rows_true)
@@ -141,3 +264,4 @@ class BNReasoner:
                 rows_true[child_cpt == True], 'p']
             print("child cpt after")
             print(child_cpt)
+"""
