d1977c39971fe09755be4e320974ad8ddbcc7813
lefkothea1 <lefkotheapat@gmail.com>
2021-12-21 16:45:52 +0100

    use case (initial FoodWaste network) with all returned vars ok!

--- use_case.py
+++ use_case.py
@@ -14,7 +14,7 @@ import random
 import itertools
 os.environ["KMP_DUPLICATE_LIB_OK"]="TRUE"
 
-
+# keep net
 keep_net = BNReasoner("testing/foodWaste.BIFXML")
 variables = keep_net.bn.get_all_variables()
 print(variables)
@@ -33,10 +33,12 @@ Developed  Country  equal  to  True,
  given  Food  Waste  equal  to true? (posterior probability)'''
 net = copy.deepcopy(keep_net) 
 evidence = pd.Series({'Food Waste':True})
-outmarg_fill = (net.marginal_distributions(['High Income Household', 'Developed Country'],evidence, "min_fill")) #values weird? or ok
-outmarg_degr = (net.marginal_distributions(['High Income Household', 'Developed Country'],evidence, "min_degree")) #NaN....
+print(net.marginal_distributions(['High Income Household', 'Developed Country'],evidence, "min_fill")) 
 
-priorProb = (net.marginal_distributions(['High Income Household', 'Developed Country'],[], "min_fill")) #values weird? or ok
+
+
+'''RQ: prior prob......'''
+print(net.marginal_distributions(['High Income Household', 'Developed Country'],[], "min_fill")) #
  
  
  
@@ -44,15 +46,15 @@ priorProb = (net.marginal_distributions(['High Income Household', 'Developed Cou
 what are themost likely values of all the other model variables (MPE)---changed it!'''
 evidence = pd.Series({'Developed Country': True,  'High Temperature': True, 'High Income Household': True}) #when only foodwaste= T then no truth values are returned
 net = copy.deepcopy(keep_net)
-mpe = net.map_mpe([],evidence, "min_degree")  #all false, does it makes sense?
-
+print(net.map_mpe([],evidence, "min_degree"))
 
 '''RQ: Given that Food Wasteis observed to be True, which is the 
 most likely instantiation of the variables HighIncome Household 
 together with Retail Waste? (MAP)'''
-evidence = pd.Series({'Food Waste':True, 'Developed Country': True, 'Retail Waste': True}) #
+evidence = pd.Series({'Bad Storage':True, 'Developed Country': False}) #
 net = copy.deepcopy(keep_net)
-mapres = net.map_mpe(['High Income Household'],evidence, "min_fill") #no truth values returned!
+print(net.map_mpe(['Home Waste'],evidence, "random"))
+
 
 
 
